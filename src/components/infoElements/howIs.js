5923bd59d6cff3368fef6cba9724946106de4e90
Shelly Eldman <shelyeldman@gmail.com>
2019-06-13 23:16:37 +0300

    fix

--- src/components/infoElements/howIs.js
+++ src/components/infoElements/howIs.js
@@ -8,11 +8,11 @@ const HowIs = ({temperature}) => {
                     How's the weather
                 </div>
                 <div className="card-body d-flex justify-content-center align-items-center">
-                    {(temperature < 25 && temperature > 16) && <div>
+                    {(temperature <= 25 && temperature >= 16) && <div>
                         <p className="mx-2">It's Nice!</p>
                         <p><i className="fas fa-cloud-sun mx-2 weather-icon"/></p>
                     </div>}
-                    {(temperature < 15) && <div>
+                    {(temperature < 16) && <div>
                         <p className="mx-2">It's Cold!</p>
                         <p><i className="fas fa-snowflake mx-2 weather-icon"/></p>
                     </div>}
